8bb00d2066828674f4a67da5e374ac367a68a7ef
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-11-29 13:23:58 +0100

    Initial Commit
    	- Setup Rust with Rocket
    	- Added preliminary schema.json
    
    Signed-Off-By: Karim Jonas Groeller <groeller@kekx.eu>
    
      Changes to be committed:
    	new file:   .gitignore
    	new file:   Cargo.lock
    	new file:   Cargo.toml
    	new file:   src/main.rs
    	new file:   src/schema/schema.json

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+#[macro_use]
+extern crate rocket;
+use rocket::{get, http::Status, serde::json::Json};
+
+#[get("/")]
+fn hello() -> Result<Json<String>, Status> {
+    Ok(Json(String::from("Hello from rust and mongodb")))
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![hello])
+}
